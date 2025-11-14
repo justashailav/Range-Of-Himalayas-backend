763132f99e39c3b85d7b651bcb13a517689780bd
Shailav <justashailav02@gmail.com>
2025-11-14 20:57:07 +0530

    updated backend again

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,23 +1,22 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async ({ email, subject, message, attachments }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",         
-    auth: {
-      user: process.env.SMTP_MAIL,
-      pass: process.env.SMTP_PASSWORD
-    },
-    port: 465,
-    secure: true,               
-  });
+  try {
+    await resend.emails.send({
+      from: process.env.RESEND_FROM,
+      to: email,
+      subject,
+      html: message,
+      attachments: attachments?.map((file) => ({
+        filename: file.filename,
+        content: file.content,
+      })),
+    });
 
-  const mailOptions = {
-    from: `"Range of Himalayas" <${process.env.SMTP_MAIL}>`,
-    to: email,
-    subject,
-    html: message,
-    attachments,
-  };
-
-  await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Email Error →", error);
+  }
 };
