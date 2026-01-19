eec4162d71590182a785441f90b4929390024d92
Shailav <justashailav02@gmail.com>
2026-01-19 09:46:25 +0530

    first commit

--- routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { chatWithAI } from "../controllers/chatComtroller.js";
-
-
-const router = express.Router();
-
-router.post("/", chatWithAI);
-
-export default router;
